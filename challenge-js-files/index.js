43aa348548273af78d32418edad72fd27d128362
RomainM27 <70415526+RomainM27@users.noreply.github.com>
2021-03-08 16:11:09 +0100

    index.js update

--- challenge-js-files/index.js
+++ challenge-js-files/index.js
@@ -1,62 +1,37 @@
-let table2 = document.getElementById("table2");
+const table1 = document.getElementById("table1"); // table 2
+const table2 = document.getElementById("table2"); // table 2
+let data_table1 = []; // data de la table2
+let data_table2 = []; // data de la table2
+getTable2InJson()
+console.log(data_table2);
+getTable1InJson()
 
-console.log(table2)
+function getTable1InJson(){
+    let country = table1.children[1].children[0].children[1].innerHTML; // le pays
+    let y1 = table1.children[2].children[0].children[2].innerHTML; 
+    let y2 = table1.children[2].children[0].children[3].innerHTML; 
+    let y3 = table1.children[2].children[0].children[4].innerHTML; 
+    let y4 = table1.children[2].children[0].children[5].innerHTML; 
+    let y5 = table1.children[2].children[0].children[6].innerHTML; 
+    let y6 = table1.children[2].children[0].children[7].innerHTML; 
+    let y7 = table1.children[2].children[0].children[8].innerHTML; 
+    let y8 = table1.children[2].children[0].children[9].innerHTML; 
+    let y9 = table1.children[2].children[0].children[10].innerHTML; 
+    let y10 = table1.children[2].children[0].children[11].innerHTML;
+    let y11 = table1.children[2].children[0].children[12].innerHTML; 
+    console.log(country,y1,y2,y3,y4,y5,y6,y7,y8,y9,y10,y11)
+}
 
-let data =[
-    {
-        "Country": "Latvia",
-        "2007–09": 312,
-        "2010–12": 312
-    },
-    {
-        "Country": "Lithuania",
-        "2007–09": 247,
-        "2010–12": 307
-    },
-    {
-        "Country": "Estonia",
-        "2007–09": 266,
-        "2010–12": 253
-    },
-    {
-        "Country": "Czech Republic",
-        "2007–09": 198,
-        "2010–12": 217
-    },
-    {
-        "Country": "Poland",
-        "2007–09": 228,
-        "2010–12": 214
+
+function getTable2InJson(){
+    let country = table2.children[1].children[0].children[1].innerHTML; // le pays
+    let y1 = table2.children[1].children[0].children[2].innerHTML; // la premiere anne
+    let y2 = table2.children[1].children[0].children[3].innerHTML; // la 2eme annee
+    for (let i = 0; i < table2.children[2].children.length; i++) {
+        data_table2.push({
+            [country]:table2.children[2].children[i].children[1].innerHTML,
+            [y1]:table2.children[2].children[i].children[2].innerHTML,
+            [y2]:table2.children[2].children[i].children[3].innerHTML
+        });
     }
-]
-
-
-var svg = dimple.newSvg("#chartContainer", 590, 400);
-  var myChart = new dimple.chart(svg, data);
-  myChart.setBounds(60, 30, 510, 330)
-  myChart.addCategoryAxis("x", ["2007–09", "2010–12"]);
-  myChart.addMeasureAxis("y", "2010–12");
-  myChart.addSeries("Country", dimple.plot.bar);
-  myChart.addLegend(65, 10, 510, 20, "right");
-  myChart.draw();
-/*
-<html>
-<div id="chartContainer">
-  <script src="/lib/d3.v4.3.0.js"></script>
-  <script src="http://dimplejs.org/dist/dimple.v2.3.0.min.js"></script>
-  <script type="text/javascript">
-    var svg = dimple.newSvg("#chartContainer", 590, 400);
-    d3.tsv("/data/example_data.tsv", function (data) {
-      var myChart = new dimple.chart(svg, data);
-      myChart.setBounds(60, 30, 510, 330)
-      myChart.addCategoryAxis("x", ["Price Tier", "Channel"]);
-      myChart.addMeasureAxis("y", "Unit Sales");
-      myChart.addSeries("Channel", dimple.plot.bar);
-      myChart.addLegend(65, 10, 510, 20, "right");
-      myChart.draw();
-    });
-  </script>
-</div>
-</html>
-
-*/
+}
