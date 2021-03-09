6ea70623e8b5eb55cc2484bf9143590181109857
Adrien Callewaert <76485285+AdrienCallewaert@users.noreply.github.com>
2021-03-09 12:00:17 +0100

    Update all.js

--- challenge-js-files/assets/js/all.js
+++ challenge-js-files/assets/js/all.js
@@ -28,112 +28,7 @@ let data2011 = data_table1.splice(0, tailleTable1);
 let data2012 = data_table1.splice(0, tailleTable1);
 
 
-var config = {
-    type: 'line',
-    data: {
-        labels: dataPays,
-        datasets: [{
-            label: '2002',
-            backgroundColor:'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: data2002,
-   
-        }, {
-            label: '2003',
-      
-            backgroundColor: 'rgb(255, 159, 64)',
-            borderColor:'rgb(255, 159, 64)',
-            data: data2003,
-        }
-        , {
-            label: '2004',
-            fill: false,
-            backgroundColor:'rgb(255, 205, 86)',
-            borderColor:'rgb(255, 205, 86)',
-            data: data2004,
-        }
-        , {
-            label: '2005',
-            fill: false,
-            backgroundColor: 'rgb(75, 192, 192)',
-            borderColor:'rgb(75, 192, 192)',
-            data: data2005,
-        }, {
-            label: '2006',
-            fill: false,
-            backgroundColor: 'rgb(54, 162, 235)',
-            borderColor: 'rgb(54, 162, 235)',
-            data: data2006,
-        }, {
-            label: '2007',
-            fill: false,
-            backgroundColor:'rgb(153, 102, 255)',
-            borderColor:'rgb(153, 102, 255)',
-            data: data2007,
-        }, {
-            label: '2008',
-            fill: false,
-            backgroundColor: 'rgb(201, 203, 207)',
-            borderColor:'rgb(201, 203, 207)', // cahnger apres
-            data: data2008,
-        }, {
-            label: '2009',
-            fill: false,
-            backgroundColor: 'rgb(201, 203, 207)',
-            borderColor:'rgb(201, 203, 207)',
-            data: data2009,
-        }, {
-            label: '2010',
-            fill: false,
-            backgroundColor:'rgb(201, 203, 207)',
-            borderColor: 'rgb(201, 203, 207)',
-            data: data2010,
-        }, {
-            label: '2011',
-            fill: false,
-            backgroundColor:'rgb(201, 203, 207)',
-            borderColor: 'rgb(201, 203, 207)',
-            data: data2011,
-        }, {
-            label: '2012',
-            fill: false,
-            backgroundColor: 'rgb(201, 203, 207)',
-            borderColor: 'rgb(201, 203, 207)',
-            data: data2012,
-        }]   
-    },
-    options: {
-        responsive: true,
-        title: {
-            display: true,
-            text: 'Chart.js Line Chart'
-        },
-        tooltips: {
-            mode: 'index',
-            intersect: false,
-        },
-        hover: {
-            mode: 'nearest',
-            intersect: true
-        },
-        scales: {
-            xAxes: [{
-                display: true,
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Month'
-                }
-            }],
-            yAxes: [{
-                display: true,
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Value'
-                }
-            }]
-        }
-    }
-};
+
 
 
 const table2 = document.getElementById("table2"); // table 2
@@ -163,6 +58,86 @@ let barChartData = {
 
 };
 
+
+let barChartData1 = {
+    labels:dataPays,
+    datasets: [{
+        label: '2002',
+        backgroundColor:'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: data2002,
+
+    }, {
+        label: '2003',
+  
+        backgroundColor: 'rgb(255, 159, 64)',
+        borderColor:'rgb(255, 159, 64)',
+        data: data2003,
+    }
+    , {
+        label: '2004',
+        fill: false,
+        backgroundColor:'rgb(255, 205, 86)',
+        borderColor:'rgb(255, 205, 86)',
+        data: data2004,
+    }
+    , {
+        label: '2005',
+        fill: false,
+        backgroundColor: 'rgb(75, 192, 192)',
+        borderColor:'rgb(75, 192, 192)',
+        data: data2005,
+    }, {
+        label: '2006',
+        fill: false,
+        backgroundColor: 'rgb(54, 162, 235)',
+        borderColor: 'rgb(54, 162, 235)',
+        data: data2006,
+    }, {
+        label: '2007',
+        fill: false,
+        backgroundColor:'rgb(153, 102, 255)',
+        borderColor:'rgb(153, 102, 255)',
+        data: data2007,
+    }, {
+        label: '2008',
+        fill: false,
+        backgroundColor: 'rgb(201, 203, 207)',
+        borderColor:'rgb(201, 203, 207)', // cahnger apres
+        data: data2008,
+    }, {
+        label: '2009',
+        fill: false,
+        backgroundColor: 'rgb(201, 203, 207)',
+        borderColor:'rgb(201, 203, 207)',
+        data: data2009,
+    }, {
+        label: '2010',
+        fill: false,
+        backgroundColor:'rgb(201, 203, 207)',
+        borderColor: 'rgb(201, 203, 207)',
+        data: data2010,
+    }, {
+        label: '2011',
+        fill: false,
+        backgroundColor:'rgb(201, 203, 207)',
+        borderColor: 'rgb(201, 203, 207)',
+        data: data2011,
+    }, {
+        label: '2012',
+        fill: false,
+        backgroundColor: 'rgb(201, 203, 207)',
+        borderColor: 'rgb(201, 203, 207)',
+        data: data2012,
+    }]   
+}
+
+
+
+
+
+
+
 function getTable2InJson(){
     // les enfants de tr 4
     for (let i = 1; i < table2.children[2].children[1].children.length; i++) {
@@ -177,7 +152,41 @@ function getTable2InJson(){
 window.onload = function () {
 
     let ctx1 = document.getElementById('canvas1').getContext('2d');
-    window.myLine = new Chart(ctx1, config);
+    window.myLine = new Chart(ctx1, {
+            type: 'line',
+            data: barChartData1,
+            options: {
+                responsive: true,
+                title: {
+                    display: true,
+                    text: 'Chart.js Line Chart'
+                },
+                tooltips: {
+                    mode: 'index',
+                    intersect: false,
+                },
+                hover: {
+                    mode: 'nearest',
+                    intersect: true
+                },
+                scales: {
+                    xAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Month'
+                        }
+                    }],
+                    yAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Value'
+                        }
+                    }]
+                }
+            }
+    });
 
     let ctx2 = document.getElementById('canvas2').getContext('2d');
     window.myBar = new Chart(ctx2, {
